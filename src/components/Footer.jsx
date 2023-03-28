a3d9ca6d23dcfedc5f66604bbea88df0e3137eed
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2023-03-28 19:29:11 +0200

    Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+        <h6>develope by Ricard Montfort - Ironhack 2023</h6>
+    </div>
+  )
+}
