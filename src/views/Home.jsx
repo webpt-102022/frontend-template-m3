2338faaac9d3a52918ec406da004e83e4f98ad51
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2023-04-07 18:18:44 +0200

    work in progress views/Home.jsx

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,10 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import KnowledgeCard from "../components/KnowledgeCard";
-
-
+import AddKnowledgeForm from '../components/AddKnowledgeForm';
+import SearchInput from '../components/SearchInput';
+import knowledgeService from '../services/knowledgeService';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 
 export default function Home() {
+  const [ knowledges, setKnowledges ] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+  const [showForm, setShowForm] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+
+
+
+
   return (
     <div>
       <h1>Home coBrain</h1>
