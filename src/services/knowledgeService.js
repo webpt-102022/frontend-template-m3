9cae13f3c7482a2221161297cc42aa105f70be07
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2023-04-07 13:47:22 +0200

    work in progress

--- src/services/knowledgeService.js
+++ src/services/knowledgeService.js
@@ -5,27 +5,35 @@ class KnowledgeService {
         this.api = axios.create({
             baseURL: `${process.env.REACT_APP_BACKEND_URL}/knowledges`
         });
-    }
+
+        this.api.interceptors.request.use(config => {
+            const storedToken = localStorage.getItem('authToken');
+            if (storedToken) {
+                config.headers = { Authorization: `Bearer ${storedToken}` };
+            }
+            return config;
+        });
+    };
 
     getKnowledges() {
         return this.api.get('/').then(({ data }) => data).catch(err => console.error(err))
-    }
+    };
 
     getKnowledge(id) {
         return this.api.get(`/${id}`).then(({ data }) => data).catch(err => console.error(err))
-    }
+    };
 
     createKnowledge(body) {
         return this.api.post('/', body).then(({ data }) => data).catch(err => console.error(err))
-    }
+    };
 
     editKnowledge(id, body) {
         return this.api.put(`/${id}`, body).then(({ data }) => data).catch(err => console.error(err))
-    }
+    };
 
     deleteKnowledge(id) {
         return this.api.delete(`/${id}`).then(({ data }) => data).catch(err => console.error(err))
-    }
+    };
 
   // async getKnowledges2() {
   //   try {
@@ -36,7 +44,7 @@ class KnowledgeService {
   //   }
   // }
 
-}
+};
 
 const knowledgeService = new KnowledgeService();
 export default knowledgeService;
