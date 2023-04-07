683d7886854807ec2625f7e14cc57e2f772ae77e
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2023-04-07 19:26:46 +0200

    work in progress views/Home.jsx

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -15,11 +15,109 @@ export default function Home() {
   const navigate = useNavigate();
 
 
+  const getKnowledges = async () => {
+    try {
+      //const response = await axios.get('http://localhost:8080/knowledges')
+      const response = await knowledgeService.getKnowledges();
+      setKnowledges(response)
+      setLoading(false)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    getKnowledges()
+  }, [])
 
 
+  const handleSortByTime = () => {
+    const sortByTime = [...knowledges].sort((a, b) => a.timeOfActivity - b.timeOfActivity);
+    setKnowledges(sortByTime);
+  }
+
+
+
+  const handleFilterCategory = () => {
+    const filteredKnowledges = knowledges.filter(elem => elem.category === 'Music' /* || === 'Cooking' || === 'Health' || === 'Sport' || === 'Crafts' || === 'Circus' || === 'Languages' || === 'Animals' || === 'Others' */ );
+    setKnowledges(filteredKnowledges);
+  }
+
+
+const handleAddKnowledge = async (newKnowledgeData) => {
+  try {
+    const newKnowledge = await knowledgeService.createKnowledge(newKnowledgeData);
+    //const newKnowledge = await axios.post('http://localhost:8080/knowledges', newKnowledgeData);
+    navigate(`/knowledges/${newKnowledge._id}`)
+  } catch (error) {
+    console.error(error)
+  }
+  // Before integration with backend
+  // const knowledgeWithId = { ...newKnowledgeData, _id: knowledges.length + 1 }
+  // setKnowledges([...knowledges, knowledgeWithId]);
+}
+
+const handleShowKnowledge = () => {
+  setShowForm(prev => !prev)
+}
+
+const handleSearch = (value) => {
+  console.log('Search', value)
+  setSearchValue(value);
+}
+
+const handleDelete = async (knowledgeId) => {
+  try {
+    //const deletedKnowledge = await axios.delete(`http://localhost:8080/knowledges/${knowledgeId}`);
+    const deletedKnowledge = await knowledgeService.deleteKnowledge(knowledgeId);
+    console.log(deletedKnowledge);
+  } catch (error) {
+    console.error(error)
+  } finally {
+    getKnowledges()
+  }
+  // const cleanKnowledges = [...knowledges].filter(elem => elem._id !== knowledgeId)
+  // setKnowledges(cleanKnowledges);
+}
+
+/*
   return (
     <div>
       <h1>Home coBrain</h1>
     </div>
   )
 }
+
+*/
+
+
+
+  return (
+  /*<div>
+      <h1>Home coBrain</h1>
+    </div>
+    */
+    <>
+      <h1>Home coBrain</h1>
+      {loading && <p>Loading...</p>}
+      {!loading &&
+        (<div className="app">
+          <div className="search_container">
+            <SearchInput handleSearchValue={handleSearch} />
+          </div>
+          <div className="action_container">
+            <button className="btn" onClick={handleSortByTime}>Sort by Time</button>
+            <button className="btn" onClick={handleFilterCategory}>Show only Music</button>
+            <button className="btn" onClick={handleShowKnowledge}>{showForm ? 'Hide' : 'Show'}</button>
+          </div>
+          {showForm && <AddKnowledgeForm handleAddKnowledge={handleAddKnowledge} />}
+          <div className="card_container">
+            {knowledges.filter(elem => elem.title.toLowerCase().includes(searchValue.toLowerCase()) || elem.description.toLowerCase().includes(searchValue.toLowerCase()))
+              .map(elem => {
+                return <KnowledgeCard key={elem._id} course={elem} handleDelete={handleDelete} />
+            })}
+          </div>
+        </div>)}
+    </>
+  );
+} 
